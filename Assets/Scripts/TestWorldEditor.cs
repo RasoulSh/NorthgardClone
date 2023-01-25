b4958e3a380eef3b91913bab86ff1561a0bf4c42
unknown <rasoul.shahhoseini@gmail.com>
2023-01-26 02:58:51 +0330

    :tada: add mediator package, generate a world filled by territories

--- Assets/Scripts/TestWorldEditor.cs
+++ Assets/Scripts/TestWorldEditor.cs
@@ -33,13 +33,32 @@ public class TestWorldEditor : MonoBehaviour
     private void AddTerritory()
     {
         var territoryPrefab = worldPipeline.TerritoryPrefabs.First();
-        var newTerritory = worldPipeline.InstantiateTerritory(territoryPrefab);
-        newTerritory.SetPosition(Vector3.up);
-        worldEditor.World.AddTerritory(newTerritory);
-        var naturalDistrictPrefab = worldPipeline.NaturalDistrictPrefabs.First();
-        var newNaturalDistrict =
-            worldPipeline.InstantiateNaturalDistrict(naturalDistrictPrefab);
-        newNaturalDistrict.SetPosition(Vector3.up * 1.5f);
-        newTerritory.AddNaturalDistrict(newNaturalDistrict);
+        var worldBounds = worldPipeline.World.Data.Bounds;
+        var territoryBounds = territoryPrefab.Bounds;
+        var territorySizeX = territoryBounds.size.x;
+        var territorySizeZ = territoryBounds.size.z;
+        var startPosition = worldBounds.min + territoryBounds.extents + (Vector3.up * (worldBounds.size.y + territoryBounds.size.y + 0.1f));
+        var minX = startPosition.x;
+        var minZ = startPosition.z;
+        var maxX = worldBounds.extents.x;
+        var maxZ = worldBounds.extents.z;
+        var pos = startPosition;
+        for (float x = minX; x < maxX; x += territorySizeX)
+        {
+            for (float z = minZ; z < maxZ; z += territorySizeZ)
+            {
+                pos.x = x;
+                pos.z = z;
+                var newTerritory = worldPipeline.InstantiateTerritory(territoryPrefab);
+                newTerritory.SetPosition(pos);
+                worldEditor.World.AddTerritory(newTerritory);   
+            }
+        }
+
+        // var naturalDistrictPrefab = worldPipeline.NaturalDistrictPrefabs.First();
+        // var newNaturalDistrict =
+        //     worldPipeline.InstantiateNaturalDistrict(naturalDistrictPrefab);
+        // newNaturalDistrict.SetPosition(Vector3.up * 1.5f);
+        // newTerritory.AddNaturalDistrict(newNaturalDistrict);
     }
 }
